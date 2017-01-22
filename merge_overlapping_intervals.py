ea07ba782ad2b89c10517d7459a28b58592d06b5
Yask Srivastava <yask123@gmail.com>
2017-01-22 19:53:52 +0530

    updated

--- merge_overlapping_intervals.py
+++ merge_overlapping_intervals.py
@@ -4,7 +4,7 @@ Given a collection of intervals, merge all overlapping intervals.
 For example:
 
 Given [1,3],[2,6],[8,10],[15,18],
-
+cl
 return [1,6],[8,10],[15,18].
 
 Make sure the returned intervals are sorted.
