5d4223a91ec29e0941f4f178ac2878fbab086290
Yask Srivastava <yask123@gmail.com>
2017-01-23 16:42:11 +0530

    update

---
+++ sqrt.py
@@ -0,0 +1,23 @@
+
+
+
+def sqrt(number):
+
+    low = 1
+    high = number - 1
+
+    while low <= high:
+
+        mid  = (low + high) /2
+
+        if mid * mid == number:
+            return  mid
+
+        elif mid * mid > number:
+            high = mid - 1
+        else:
+            low = mid + 1
+
+    return low - 1
+
+print sqrt(15)
